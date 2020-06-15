db225bf8ab21956fdc260dc3613370b90025fa76
spencerhhubert <spencerhhubert@gmail.com>
2020-06-15 00:46:03 -0700

    worked on footer some more

--- bootstrap/includes/footer.php
+++ bootstrap/includes/footer.php
@@ -1,22 +1,48 @@
     <div class="container footer text-light">
         <div class="row">
             <div class="col-12 col-md-6 d-flex justify-content-center align-items-center item">
-                <a href="https://lan.lego.com/"><img src="images/Recognized-LOC_White_2019.png" style="max-width: 400px; width: 100%;"></a>
+                <a href="https://lan.lego.com/" target="_blank"><img src="images/Recognized-LOC_White_2019.png" style="max-width: 400px; width: 100%;"></a>
             </div>
             <div class="col-12 col-md-6 row item">
-                <div class="col-4 col-md-3">
-                    <p>Email</p>
-                    <p>Merch</p>
-                    <p>Donate</p>
+
+                <div class="col-12 row">
+                    <div class="col-4 d-flex align-items-center item"><h3>Email</h3></div>
+                    <div class="col d-flex align-items-center item">
+                        <a href="mailto: rebellegousergroup@gmail.com" target="_blank">
+                            <i class="fas fa-envelope align-middle" aria-hidden="true"></i>
+                        </a>
+                        <a href="mailto: rebellegousergroup@gmail.com" target="_blank">
+                            <p class="align-middle">rebellegousergroup@gmail.com</p>
+                        </a>
+                    </div>
                 </div>
-                <div class="col-8 col-md-9">
-                    <a href="mailto: rebellegousergroup@gmail.com"><p class="align-middle">rebellegousergroup@gmail.com</p></a>
-                    <a href="https://teespring.com/stores/rebellug-merch"><p class="align-middle">Official Teespring</p></a>
-                    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=48RGJ37LWQBAG"><p class="align-middle">PayPal</p></a>
+
+                <div class="col-12 row">
+                    <div class="col-4 d-flex align-items-center item"><h3>Merch</h3></div>
+                    <div class="col d-flex align-items-center item">
+                        <a href="https://teespring.com/stores/rebellug-merch" target="_blank">
+                            <i class="fas fa-tshirt align-middle" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://teespring.com/stores/rebellug-merch" target="_blank">
+                            <p class="align-middle">Official Teespring</p>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-12 row">
+                    <div class="col-4 d-flex align-items-center item"><h3>Donate</h3></div>
+                    <div class="col d-flex align-items-center item">
+                        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=48RGJ37LWQBAG" target="_blank">
+                            <i class="fab fa-paypal align-middle" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=48RGJ37LWQBAG" target="_blank">
+                            <p class="align-middle">PayPal</p>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="item"><p style="text-align: center;">© RebelLUG. All rights reserved. LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize, or endorse this site.</p></div>
+        <div class="item copyright"><p style="text-align: center;">© RebelLUG. All rights reserved. LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize, or endorse this site.</p></div>
     </div>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
