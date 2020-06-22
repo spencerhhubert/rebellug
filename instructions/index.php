0552ed0e69e91b9c0690769086db83b599f66c67
spencerhhubert <spencerhhubert@gmail.com>
2020-06-21 17:11:12 -0700

    instructions gallery loading

--- instructions/index.php
+++ instructions/index.php
@@ -1,14 +1,52 @@
-<?php include "../config.php" ?>
-<?php include SITE_ROOT . "/includes/header.php" ?>
+<?php
+function printInstructionsModule($title, $builder_name, $builder_handle, $price, $pic_main) {
+    print_r('
+        <div class="col-md-4 col-md">
+            <div class="gallery-item">
+                <form method="get" action="moc.php" class="inline">
+                    <input type="hidden" name="id" value="' . $title . '">
+                    <button type="submit" class="link-button" style="width: 100%;">
+                        <a href="#"><h3>' . $title . '</h3></a>
+                    </button>
+                </form>
+                <p>' . $builder_name . ' - ' . $builder_handle . '</p>
+                <p><b>$' . $price . ' USD</b></p>
+                <form method="get" action="moc.php" class="inline">
+                    <input type="hidden" name="id" value="' . $title . '">
+                    <button type="submit" class="link-button">
+                        <a href="#"><img src="images/' . $pic_main . '" width="100%"></a>
+                    </button>
+                </form> 
+            </div> 
+        </div>
+    ');
+}
+
+include "../config.php";
+include SITE_ROOT . "/includes/header.php";
+?>
 <div class="content">
     <div class="container">
         <div class="box">
-            <form method="get" action="moc.php" class="inline">
-            <input type="hidden" name="id" value="HAVw A6 Juggernaut">
-            <button type="submit" class="link-button">
-                <a href="some_page"><img src="https://rebellug.com/instructions/juggernaut/image0.jpg" width="50%"></a>
-            </button>
-            </form>
+            <div class="row">
+                <?php
+                    $result = mysqli_query($conn, "SELECT * FROM instructions");
+                    $resultCheck = mysqli_num_rows($result);
+
+                    if ($resultCheck > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $title = $row['title'];
+                            $builder_name = $row['builder_name'];
+                            $builder_handle = $row['builder_handle'];
+                            $price = $row['price'];
+                            $pic_main = $row['pic_main'];
+                            printInstructionsModule($title, $builder_name, $builder_handle, $price, $pic_main);
+                        }
+                    } else {
+                        echo "There was an error loading the instructions";
+                    }
+                ?>
+            </div>
         </div>
     </div>
 </div>
