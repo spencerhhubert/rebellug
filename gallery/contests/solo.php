d00f5d438ad25bcbaf0d3d70d36189c15b63bb21
spencerhhubert <spencerhhubert@gmail.com>
2020-08-15 16:00:10 -0700

    solo

---
+++ gallery/contests/solo.php
@@ -0,0 +1,49 @@
+<?php
+include "../../config.php";
+$title = "Solo";
+include SITE_ROOT . "/includes/header.php";
+?>
+
+<div class="content">
+    <div class="container">
+        <div class="box collaboration">
+            <?php echo contestOverAlert(); ?>
+            <div class="text-center">
+                <h1 class="header">Solo</h1>
+                <img src="images/solo.jpg" class="logo">
+            </div>
+            <h2>The Challenge</h2>
+            <ul>
+                <li>Build a custom recreation of a scene from Solo: A Star Wars Story</li>
+                <li>Build at your preferred scale (minifig-scale or micro-scale)</li>
+                <li>Build larger than 16x16 studs</li>
+                <li>Complete and post before July 14, 2018 at 11:59pm PST</li>
+                <li>You will be judged on creativity and attention to detail</li>
+            </ul>
+            <h2>The Rules</h2>
+            <ul>
+                <li>Post either three pictures of your MOC to your Instagram with the hashtag <a href="https://www.instagram.com/explore/tags/rebellugsolo/?hl=en">#rebellugsolo</a> or post a YouTube video of your MOC</li>
+                <li>Submit links to either the Instagram pictures or the YouTube video in the form found at solo.rebellug.com</li>
+                <li>Your MOC must be uploaded after the beginning of the contest (no old MOCs)</li>
+                <li>Use only LEGO elements, however third party-custom blasters or minifigures (CAC, EclipseGrafx) are allowed</li>
+                <li>There is no limitation on location, the contest is international</li>
+                <li>You may enter as many times as you like, but only one MOC will win</li>
+            </ul>
+
+            <h2>The Results</h2>
+            <ul>
+                <li>
+                    <b>Winner: <a href=https://www.flickr.com/photos/brickninja/28306565277/in/dateposted/ target=_blank>Brick Ninja</a></b>
+                    <a data-flickr-embed="true" href="https://www.flickr.com/photos/brickninja/28306565277/in/dateposted/" title="Solo: A Star Wars Story, Chase on Corellia"><img src="https://live.staticflickr.com/1806/28306565277_02dc1e8428_k.jpg" width="100%" alt="Solo: A Star Wars Story, Chase on Corellia"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+                    <ul>
+                        <li>Prize:</li>
+                        <li>75212 LEGO Star Wars Kessel Run Millenium Falcon</li>
+                    </ul>
+                </li>
+            </ul> 
+            <p>Participants attending any RebelLUG Events will also receive Participation bricks (while supplies last). Thank you to <a href="http://www.eclipsebricks.com//">eclipseGRAFX</a> for sponsoring this contest.</p>
+        </div>
+    </div>
+</div>
+
+<?php include SITE_ROOT . "/includes/footer.php" ?>
