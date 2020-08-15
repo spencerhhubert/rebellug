bbc9cea7e84e13cfe8eaee3ea4e818dd33037fb0
spencerhhubert <spencerhhubert@gmail.com>
2020-08-15 15:55:55 -0700

    grid wars

---
+++ gallery/contests/gridwars.php
@@ -0,0 +1,66 @@
+<?php
+include "../../config.php";
+$title = "GRID Wars: Episode IV: A New Contest";
+include SITE_ROOT . "/includes/header.php";
+?>
+
+<div class="content">
+    <div class="container">
+        <div class="box collaboration">
+            <?php echo contestOverAlert(); ?>
+            <div class="text-center">
+                <h1 class="header">GRID Wars: Episode IV: A New Contest</h1>
+                <img src="images/gridwarslogo.png" class="logo">
+                <img src="images/gridwars.jpg" class="logo">
+            </div>
+            <h2>The Challenge</h2>
+            <ul>
+                <li>Build a custom build of a Star Wars Vehicle, using the Tron Aesthetic (Black with bright neon colours)</li>
+                <li>Any Vehicle from the Star Wars universe is allowed - Movie, TV, Comics, Books, or even Extended Universe. Please include a reference photo if your chosen vehicle is obscure. </li>
+                <li>Build must be new, built after Oct 15, 2018</li>
+                <li>No size limit on your builds</li>
+                <li>Builds can be any scale - Minifig / Micro / 1:1</li>
+                <li>Brick built backgrounds are not required</li>
+            </ul>
+            <h2>The Rules</h2>
+            <ul>
+                <li>Submit one (1) photo to this <a href="https://www.flickr.com/groups/3940745@N24/discuss/72157696561218020/">GRID WARS Flickr Group</a></li>
+                <li>Please use #GridWarsIV hastag in other social posts </li>
+                <li>No digital entries, only LEGO elements can be used, however third party blasters/minifigs (CAC, EclipseGrafix etc) are allowed </li>
+                <li>You may create as many entries as you like, but you may only place once</li>
+                <li>Submit your build before Nov 15, 2018. <b>This is the contest's deadline!</b></li>
+            </ul>
+
+            <h2>The Prizes</h2>
+            <ul>
+                <li>
+                    <b>First Place</b>
+                    <ul>
+                        <li>75205 - Mos Eisley Cantina™</li>
+                        <li>21314 TRON: Legacy</li>
+                        <li>Participation Brick / Trophy</li>
+                    </ul>
+                </li>
+                <li>
+                    <b>Second Place</b>
+                    <ul>
+                        <li>75200 Ahch-To Island™ Training</li>
+                        <li>21314 TRON: Legacy</li>
+                        <li>Participation Brick / Trophy</li>
+                    </ul>
+                </li>
+                <li>
+                    <b>Third Place</b>
+                    <ul>
+                    <li>41627 Luke Skywalker™ & Yoda™</li>
+                    <li>21314 TRON: Legacy</li>
+                    <li>Participation Brick / Trophy</li>
+                    </ul>
+                </li>
+            </ul>
+            <p>Participants attending any RebelLUG Events will also receive Participation bricks (while supplies last). Thank you to <a href="http://www.eclipsebricks.com//">eclipseGRAFX</a> for sponsoring this contest.</p>
+        </div>
+    </div>
+</div>
+
+<?php include SITE_ROOT . "/includes/footer.php" ?>
