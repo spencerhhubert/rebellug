27f1d1b00a9cc3792fceb3d342ee1280fbfe791b
spencerhhubert <spencerhhubert@gmail.com>
2020-08-15 16:15:54 -0700

    Battlefront contest 2017

---
+++ gallery/contests/battlefront.php
@@ -0,0 +1,24 @@
+<?php
+include "../../config.php";
+$title = "Battlefront";
+include SITE_ROOT . "/includes/header.php";
+?>
+
+<div class="content">
+    <div class="container">
+        <div class="box collaboration">
+            <?php echo contestOverAlert(); ?>
+            <div class="text-center">
+                <h1 class="header">Battlefront</h1>
+                <img src="images/battlefront.jpg" class="logo">
+            </div>
+            <p>The Battlefront contest was exclusive to RebelLUG members, but we encouraged our followers to vote on who the winners of each round would be. Each week a new planet from Star Wars was used as the challenge (picked by the previous week's winner) and members created vignettes taking place on that planet.</p>
+            <p>The contest started August 18, 2017 and lasted seven weeks.</p>
+            <div class="text-center">
+                <iframe width="100%" style="max-width: 560px; height: 315px;" src="https://www.youtube.com/embed/mxlQP3UrG6M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include SITE_ROOT . "/includes/footer.php" ?>Battlefront
