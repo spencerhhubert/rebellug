fb41e49d3ebcba1ae908850b81f959c9cfdd9d0d
spencerhhubert <spencerhhubert@gmail.com>
2020-08-15 16:04:43 -0700

    rogue one

---
+++ gallery/contests/rogueone.php
@@ -0,0 +1,44 @@
+<?php
+include "../../config.php";
+$title = "Rogue One";
+include SITE_ROOT . "/includes/header.php";
+?>
+
+<div class="content">
+    <div class="container">
+        <div class="box collaboration">
+            <?php echo contestOverAlert(); ?>
+            <div class="text-center">
+                <h1 class="header">Rogue One</h1>
+                <img src="images/rogueonecontest.jpg" class="logo">
+            </div>
+            <h2>The Challenge</h2>
+            <ul>
+                <li>Build your favorite scene from Rogue One</li>
+                <li>May Use 3rd party figures/accessories (brickarms or CAC) but all other pieces must be LEGO</li>
+                <li>No size requirements</li>
+            </ul>
+            <h2>The Rules</h2>
+            <ul>
+                <li>Contest closes February 1st, 2017</li>
+                <li>Submit entry by uploading a video to YouTube and email the link to: rebellegousergroup@gmail.com</li>
+                <li>MOCs will be judged based on detail, not size</li>
+                <li>RebelLUG members will vote on winner</li>
+            </ul>
+
+            <h2>The Results</h2>
+            <ul>
+                <li><b>Winner: <a href=https://youtu.be/SW0iolE8D8Q>The Brick Puke</a></b>
+                    <ul>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/SW0iolE8D8Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <li>Prize:</li>
+                        <li>2x 75164 LEGO Star Wars Rebel Trooper Battle Pack</li>
+                        <li>18x CAC Series 3 Weapons</li>
+                    </ul>
+                </li>
+            </ul>  
+        </div>
+    </div>
+</div>
+
+<?php include SITE_ROOT . "/includes/footer.php" ?>
