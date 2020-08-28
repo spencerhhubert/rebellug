565719e734fdf7ce284f00dab0810f15cc5eb99a
spencerhhubert <spencerhhubert@gmail.com>
2020-08-28 13:04:24 -0700

    blaster collab credits

--- gallery/collaborations/souncivilized.php
+++ gallery/collaborations/souncivilized.php
@@ -19,7 +19,7 @@ include SITE_ROOT . "/includes/header.php";
             <?php
 
             $builds = array("E-11 Blaster Rifle", "Princess Leia's Defender Sporting Pistol", "Han Solo's DL-44 Pistol", "Biker Scout Trooper Pistol", "CR-2 Heavy Blaster Pistol", "Padme's ELG-3A Blaster Pistol", 'Model 434 "Death Hammer"', "DC-17s Blaster Pistol", "DC-17m Interchangeable Weapon System", "DC-17 Hand Blaster", "Mandalorian WESTAR-35 Blaster Pistol", "Hera's Blurrg-1120 Pistol", "SE-44C Blaster Pistol", "E11D Blaster Carbine", "Jyn Erso's A180 Blaster Pistol", "ST-W48 Sith Trooper Carbine", "Din Djarin's Blaster Pistol");
-            $builders = array(4, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4);
+            $builders = array(32, 33, 7, 46, 21, 38, 41, 24, 29, 50, 11, 9, 43, 30, 7, 14, 17);
             $buildImages = array("01_Blank_Caleb_LegoHobbitFan_E11.jpg ",  "02_Blank_Joe_JNJ_Leia.jpg ",  "03_Blank_Nick_NickBrick_DL44.jpg ",  "04_Blank_Yannis_YRBricks_BikerScoutPistol.jpg ",  "05_Blank_Andrew_CRCT_CR2.jpg ",  "06_Blank_Caleb_MaskedBuilder_ELG3A.jpg ",  "07_bLANK_Thomas_LegionDude_Deathhammer.jpg ",  "08_Blank_Peter_GuySmiley_DC17S.jpg ",  "09_Blank_Simon_SiMocs_DC17mICWS.jpg ",  "10_Blank_Noah_H2Brick_DC17.jpg ",  "11_Blank_Spencer_LegoSpencer_Westar35.jpg ",  "12_Blank_Ryan_Goatman_Blurrg1120.jpg ",  "13_Blank_DanielBarwegen_SE44C.jpg ",  "14_Blank_Alec_AlecD_E11D.jpg ",  "15_Blank_Nick_NickBrick_A180.jpg ",  "16_Blank_Bryce_BrickbuiltReplicas_STW48.jpg ",  "17_Blank_Fin_Legofin_DinDjarin.jpg ");
 
             echo printCollaborationBuilds01($builds, $builders, $buildImages, "so-uncivilized/")
