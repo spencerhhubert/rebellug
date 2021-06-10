527a5187c85aa88f524aa7451b3b00b2af451861
spencerhhubert <33559710+spencerhhubert@users.noreply.github.com>
2021-06-10 11:15:46 -0700

    Reduce code a bit

--- includes/functions.js
+++ includes/functions.js
@@ -1,19 +1,6 @@
 function next_nth_day(day, count) {
-	source = new Date();
-	source.setDate(1);
-	let i = 1;
-	let j = 0;
-	while (source.getDay() != day || j != count) {
-		if (source.getDay() == day) {
-			j++;
-		}
-		i++;
-		source.setDate(i);
-	}
-	if (source < (new Date())) {
-		source = new Date();
-		source.setMonth(source.getMonth() + 1)
-		source.setDate(1);
+	function loop_month() {
+		source.setDate(1)
 		let i = 1;
 		let j = 0;
 		while (source.getDay() != day || j != count) {
@@ -24,6 +11,13 @@ function next_nth_day(day, count) {
 			source.setDate(i);
 		}
 	}
-	return new Date(source);
-}
+	source = new Date();
+	loop_month();
+	if (source < (new Date())) {
+		source = new Date();
+		source.setMonth(source.getMonth() + 1);
+		loop_month();
 
+	}
+	return source;
+}
